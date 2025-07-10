ce96d09c5f1b9e3dac3d06045120e30eeaa15b6d
navanken <afandinaufal67@gmail.com>
2025-07-10 19:26:34 +0700

    add keterangan_izin table

--- database/migrations/2025_07_05_080216_create_perizinan_santri_table.php
+++ database/migrations/2025_07_05_080216_create_perizinan_santri_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
@@ -18,8 +17,10 @@ return new class extends Migration
             $table->datetime('tgl_kembali');
             $table->string('penjemput');
             $table->text('keperluan');
-            $table->text('catatan')->nullable();
             $table->string('status')->default('Draft');
+            $table->text('catatan')->nullable();
+            $table->unsignedBigInteger('keterangan_izin_id')->nullable();
+            $table->string('foto_bukti')->nullable();
             $table->datetime('waktu_keluar')->nullable();
             $table->datetime('waktu_kembali')->nullable();
             $table->unsignedBigInteger('musyrif_id');
@@ -27,6 +28,7 @@ return new class extends Migration
 
             $table->foreign('santri_id')->references('id')->on('santri');
             $table->foreign('musyrif_id')->references('id')->on('musyrif');
+            $table->foreign('keterangan_izin_id')->references('id')->on('keterangan_izin');
         });
     }
 
